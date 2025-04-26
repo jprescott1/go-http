7f5e3590abc75cfe0132f717b2a9044a2499e26d
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-04-26 11:10:14 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module chirpy.gloogleegloo.com
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	mux := http.ServeMux{}
+	s := http.Server{
+		Addr:    ":8080",
+		Handler: &mux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
